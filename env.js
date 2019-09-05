7db301a6c29aaa5ec1f732e7c04e4ae552f29634
Engr. Alabi Temitope Wahab <t.banji@rocketmail.com>
2019-09-05 23:53:00 +0100

    updated

---
+++ env.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mongodb_url: process.env.MONGODB_URL,
+    jwt_secret: process.env.JWT_SECRET,
+    domain: process.env.DOMAIN,
+    api_key: process.env.API_KEY,
+    port: process.env.PORT || 6004,
+    paystack_secret_key: process.env.PAYSTACK_SECRET_KEY,
+    sendgrid_api_key: process.env.SENDGRID_API_KEY,
+    mailgun_email_sender: process.env.MAILGUN_EMAIL_SENDER,
+    admin_key: process.env.ADMIN_KEY,
+}
